7b731203a862d96cad09c141237f332b9f222ceb
Mason <mason@Masons-MacBook-Air-2.local>
2024-10-31 09:32:45 +0000

    added endpoint to get single sleeve

--- src/controllers/SleevesControllers.ts
+++ src/controllers/SleevesControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { addSleeve, findSleeves } from "../modles/SleevesModels";
+import { addSleeve, findSleeve, findSleeves } from "../modles/SleevesModels";
 
 export async function getSleeves(req: Request, res: Response, next: NextFunction) {
   try {
@@ -16,6 +16,17 @@ export async function postSleeve(req: Request, res: Response, next: NextFunction
   try {
     let sleeve = await addSleeve(sleeve_name, description, price, stock, height, width, pack_size)
     res.status(201).send({sleeve})
+  }
+  catch (err) {
+    next(err)
+  }
+}
+
+export async function getSleeve(req: Request, res: Response, next: NextFunction) {
+  const { sleeve_id} = req.params
+  try {
+    let sleeve = await findSleeve(sleeve_id)
+    res.status(200).send({sleeve})
   }
   catch (err) {
     console.log(err)
--- src/modles/SleevesModels.ts
+++ src/modles/SleevesModels.ts
@@ -1,4 +1,5 @@
 import db from "../db";
+import { checkExists } from "../utils";
 
 export async function findSleeves() {
   let sleeves = await db.query(`SELECT sleeves.*, avg(reviews.rating) AS average_review, count(reviews.rating) AS num_reviews
@@ -14,5 +15,19 @@ export async function addSleeve(name:string, description:string, price:any, stoc
     VALUES ($1,$2,$3,$4,$5,$6,$7)
     RETURNING *;`,
   [name, description, price, stock, height, width, pack_size])
+  return sleeve.rows[0]
+}
+
+export async function findSleeve(id: any) {
+  await checkExists('sleeves', 'sleeve_id', [id])
+  let sleeve = await db.query(
+    `SELECT sleeves.*, avg(reviews.rating) AS average_review, count(reviews.rating) AS num_reviews
+     FROM sleeves
+     LEFT JOIN reviews ON (sleeves.sleeve_id = reviews.entity_id AND reviews.entity_type = 'sleeves')
+     WHERE sleeves.sleeve_id = $1
+     GROUP BY sleeves.sleeve_id
+     ;`,
+    [id]
+  );
   return sleeve.rows[0]
 }
